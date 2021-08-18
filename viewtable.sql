dab4e20455bee999b89c7f3b8bdd0caccbf09638
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-08-18 13:00:36 -0400

    Query for viewing db data

---
+++ viewtable.sql
@@ -0,0 +1,4 @@
+-- After table articles is created in pgAdmin, jupiter notebook Exploratory.ipynb was used to load the csv data to the database
+-- To view the data
+select * from articles
+order by index asc
