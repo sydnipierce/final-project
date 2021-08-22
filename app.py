b1f2588a2c8b79e8367f2c39d8b9a4356d49288b
Sydni <sydnimpierce@gmail.com>
2021-08-22 09:00:43 -0800

    app shell

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect
 
+import os
 import numpy as np
 import pandas as pd
+import pickle
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+@app.route('/', methods=["POST"])
+def home():
+    model = os.path.join("Data", "model.sav")
+    loaded_model = pickle.load(open(model, 'rb'))
+
+    if request.method == "POST":
+        result = loaded_model.score(X_test, Y_test)
+        print(result)
+
+    return render_template("index.html", results=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
