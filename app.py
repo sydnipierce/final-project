abad747e4649b55cf664bbb2e9c9cce6e7371354
Sydni <sydnimpierce@gmail.com>
2021-08-23 17:53:54 -0800

    added updated html, app.py

--- app.py
+++ app.py
@@ -3,21 +3,27 @@ from flask import Flask, render_template, jsonify, request, redirect
 import os
 import numpy as np
 import pandas as pd
-import pickle
+from urllib.request import urlopen
+import cloudpickle as cp
+
+from wordcloud import WordCloud
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST"])
+@app.route('/', methods=["GET", "POST"])
 def home():
-    model = os.path.join("Data", "model.sav")
-    loaded_model = pickle.load(open(model, 'rb'))
+    wc = WordCloud()
+    # img = wc.generate_from_text(' '.join(tokenized_word_2))
+    # img.to_file('worcloud.jpeg')
 
+    loaded_model = cp.load(urlopen("https://butlerunit22.s3.us-east-2.amazonaws.com/class_model.sav", 'rb'))
+    print(loaded_model)
+    user_input = request.form["variable"]
+    print(user_input)
     if request.method == "POST":
-        user_input = request.form['variable']
         result = loaded_model.score(user_input)
         print(result)
-
     return render_template("index.html", results=result)
 
 if __name__ == "__main__":
